f895e6d99d847a7e996e5254007fd40dba82f60a
mmk <mmkkarbalaei@gmail.com>
2024-02-12 22:23:10 +0330

    createGameBoard function

--- game.cpp
+++ game.cpp
@@ -9,6 +9,37 @@ struct GameBoard {
 };
 int boardSize;
 
+GameBoard createGameBoard(int n);
+
+
 int main(){
     return 0;
+}
+
+GameBoard createGameBoard(int n) {
+    GameBoard game;
+    game.size = n;
+    game.board = new char* [n + 1];
+    for (int i = 1; i <= n; i++) {
+        game.board[i] = new char[n + 1];
+        for (int j = 1; j <= n; j++) {
+            game.board[i][j] = ' ';
+        }
+    }
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int randomRowUser, randomColUser, randomRowComputer, randomColComputer;
+
+   
+    if (n % 2 == 0) {
+        game.board[n / 2 + 1][n / 2] = 'B';
+        game.board[n / 2][n / 2 + 1] = 'R';
+    }
+    else {
+        game.board[n / 2 + 1][n / 2] = 'B';
+        game.board[n / 2 + 1][n / 2 + 1] = 'R';
+
+    }
+
+    return game;
 }
